40dde756286cbd1e632bdcaa9fe8e6a74c44c327
Rohit <rohitpathak987@gmail.com>
2019-05-29 11:30:53 +0100

    Prototype Solution 1
    Added cube movement and shadowmovement

---
+++ Prototype1/Assets/Scenes/Material/Player_Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+    internal Rigidbody rb;
+
+    [SerializeField]
+    private float speed;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x_value = Input.GetAxis("Horizontal") * -speed;
+
+        Vector3 vel = rb.velocity;
+
+        vel.x = x_value;
+        rb.velocity = vel;
+
+    }
+}
---
+++ Prototype1/Assets/Scenes/Material/Shadow_Moveemnt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow_Moveemnt : MonoBehaviour {
+    internal Rigidbody rb;
+
+    [SerializeField]
+    private float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x_value = Input.GetAxis("Horizontal_1") * speed;
+       
+
+        Vector3 vel = rb.velocity;
+
+        
+        vel.x = x_value;
+        rb.velocity = vel;
+
+    }
+}
