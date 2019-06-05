5a9c29f3311f501d6e189d1e07e0fefbf319a487
JordanD493 <46814724+JordanD493@users.noreply.github.com>
2019-06-05 10:29:28 +0100

    Adapted Shadows (Jordan) and Altered Player (Michele)

--- Prototype1/Assets/Scripts/Player_Movement.cs
+++ Prototype1/Assets/Scripts/Player_Movement.cs
@@ -6,26 +6,38 @@ public class Player_Movement : MonoBehaviour {
 
     internal Rigidbody rb;
 
-    [SerializeField]
-    private float speed;
+    private bool movementActivated;
+
+    [Tooltip("X positive, Y and Z zero if you just want the player to be pushed to the right. Press M to make the player move")]
+    [SerializeField] Vector3 forceBeingApplied;
+    //private float speed;
 
 
 
     // Use this for initialization
     void Start()
     {
+        movementActivated = false;
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x_value = Input.GetAxis("Horizontal") * -speed;
+        //float x_value = Input.GetAxis("Horizontal") * -speed;
 
-        Vector3 vel = rb.velocity;
+        if (Input.GetKeyDown("m"))
+            movementActivated = true;
 
-        vel.x = x_value;
-        rb.velocity = vel;
+        //Vector3 vel = rb.velocity;
+        //vel.x = x_value;
+        //rb.velocity = vel;
 
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementActivated)
+            rb.AddForce(forceBeingApplied);
     }
 }
