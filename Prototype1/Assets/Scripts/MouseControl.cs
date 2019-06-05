774d66abbf74937a36767722aff6bce0de3c705c
Michele Busi <1702835@uad.ac.uk>
2019-06-05 14:12:48 +0100

    created boundaries for pop-ups movement

--- Prototype1/Assets/Scripts/MouseControl.cs
+++ Prototype1/Assets/Scripts/MouseControl.cs
@@ -5,15 +5,43 @@ using UnityEngine;
 public class MouseControl : MonoBehaviour
 {
 
+    // ------------------------------- ALL THE X VALUES ARE INVERTED BECAUSE THAT'S THE WAY THE SCENE WAS BUILT! GOING TO THE RIGHT DECREASES THE X!
+
     // Mouse Control Variabbles
     [SerializeField] private float mouseSensitivityX = 10;
+    [SerializeField] private float leftBoundaryOffset = 4;
+    [SerializeField] private float rightBoundaryOffset = 2;
+
+    private float leftBoundaryX;
+    private float rightBoundaryX;
 
     private bool selected = false;
 
     // Use this for initialization
     void Start()
     {
+        leftBoundaryX = gameObject.transform.position.x + leftBoundaryOffset;
+        rightBoundaryX = gameObject.transform.position.x - rightBoundaryOffset;
+    }
 
+    void OnDrawGizmosSelected()
+    {
+
+        // Draws a vertical red line on the left boundary
+        Vector3 lineStart = gameObject.transform.position;
+        lineStart.x = gameObject.transform.position.x + leftBoundaryOffset;
+        Vector3 lineTarget = lineStart;
+        lineTarget.y = 20;
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(lineStart, lineTarget);
+
+        // Draws a vertical blue line on the right boundary
+        lineStart = gameObject.transform.position;
+        lineStart.x = gameObject.transform.position.x - rightBoundaryOffset;
+        lineTarget = lineStart;
+        lineTarget.y = 20;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(lineStart, lineTarget);
     }
 
     // Update is called once per frame
@@ -24,10 +52,14 @@ public class MouseControl : MonoBehaviour
             if (!Input.GetMouseButton(0))
                 selected = false;
 
+
             float moveLR = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime;
 
-            // add left right mouse movement to the X value of the object's transform
-            transform.position += new Vector3(-moveLR, 0, 0);
+            if ((moveLR < 0 && gameObject.transform.position.x < leftBoundaryX) || (moveLR > 0 && gameObject.transform.position.x > rightBoundaryX))
+            {
+                // add left right mouse movement to the X value of the object's transform
+                transform.position += new Vector3(-moveLR, 0, 0);
+            }
         }
     }
 
---
+++ Prototype1/Assets/Scripts/TriggerLevelEnd.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLevelEnd : MonoBehaviour
+{
+
+    [Tooltip("this is needed so i can easily access the player's animatior component")]
+    [SerializeField] private GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("console works");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "EndGoal")
+        {
+            player.GetComponent<Animator>().SetBool("ReachedEnd", true);
+            Debug.Log("player hit end");
+        }
+    }
+}
