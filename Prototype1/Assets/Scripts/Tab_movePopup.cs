08bd6270120fb5d81a4f2470129fb27d9c5b6474
Michele Busi <1702835@uad.ac.uk>
2019-06-13 13:53:01 +0100

    re-commit

--- Prototype1/Assets/Scripts/Tab_movePopup.cs
+++ Prototype1/Assets/Scripts/Tab_movePopup.cs
@@ -12,17 +12,12 @@ public class Tab_movePopup : MonoBehaviour
     [Tooltip("How fast the popup moves relative to tab movement")]
     [SerializeField] private float movementSpeed = 1.0f;
 
-    // Will store slideDirection value found in moveTab script
-    TabSlideDirection slideDirection = TabSlideDirection.Horizontal;
-
     // Stores last position and current position so we can calculate how much tab has moved since last frame
     Vector3 lastPosition;
     Vector3 currentPosition;
 
     private void Start()
     {
-        Tab_moveTab moveTabScript = gameObject.GetComponent<Tab_moveTab>();
-        slideDirection = moveTabScript.slideDirection;
         currentPosition = transform.position;
         lastPosition = currentPosition;
 
@@ -34,7 +29,7 @@ public class Tab_movePopup : MonoBehaviour
         currentPosition = transform.position;
         if (currentPosition != lastPosition)
         {
-            transformThatWillBeMoved.Translate(currentPosition - lastPosition);
+            transformThatWillBeMoved.Translate((currentPosition - lastPosition) * movementSpeed);
         }
         lastPosition = currentPosition;
     }
