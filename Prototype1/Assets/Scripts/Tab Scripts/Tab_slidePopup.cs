e913f258ce0e8cdc8484590b03b00c13e1589f11
Michele Busi <1702835@uad.ac.uk>
2019-06-20 15:59:45 +0100

    Major Tab scripts redesign!!!
    now there's two kind of scripts: operateTab and operatePopup from which every script will inherit
    
    this is not just a much neater design, but it'll also allow me to easily implement scripts for rotating the tabs and the popups

---
+++ Prototype1/Assets/Scripts/Tab Scripts/Tab_slidePopup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PopupSlideDirection
+{
+    Horizontal,
+    Vertical
+}
+
+public class Tab_slidePopup : Tab_operatePopup
+{
+    // ------------------------------------------------------------------------------------------------------ INSPECTOR INTERFACE - YOU CAN SAFELY TWEAK THESE VALUES
+    [Header("")]
+    [SerializeField] public PopupSlideDirection slideDirection = PopupSlideDirection.Horizontal;
+    [Tooltip("You need to drag the parent popUp in here so that both the popup and the shadow will move")]
+    [SerializeField] private Transform slideMe;
+    [Tooltip("How fast the popup moves relative to tab movement")]
+    [SerializeField] private float movementSpeed = 1.0f;
+    // --------------------------------------------------------------------------------------------------------------------------------------- INSPECTOR INTERFACE END
+
+    private Tab_operateTab operateTabScript;
+    private float tabMovement;
+
+    private void Start()
+    {
+        operateTabScript = GetComponent<Tab_operateTab>();
+        if (operateTabScript == null)
+        {
+            Debug.Log("The object this script is attached to needs to have a script that operates the tab!");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        tabMovement = operateTabScript.getTabMovement();
+        if (tabMovement != 0)
+        {
+            switch (slideDirection)
+            {
+                case PopupSlideDirection.Horizontal:
+                    slideMe.Translate(new Vector3(tabMovement * movementSpeed, 0, 0));
+                    break;
+                case PopupSlideDirection.Vertical:
+                    slideMe.Translate(new Vector3(0, 0, tabMovement * movementSpeed));
+                    break;
+            }
+        }
+    }
+}
