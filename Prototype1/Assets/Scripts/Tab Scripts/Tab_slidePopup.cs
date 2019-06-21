e937347102baa5c329f48ca080894f9d609de1fb
JordanD493 <46814724+JordanD493@users.noreply.github.com>
2019-06-21 13:55:00 +0100

    Merge branch 'MicheleBranch_V2' into Jordan_BranchV2

--- Prototype1/Assets/Scripts/Tab Scripts/Tab_slidePopup.cs
+++ Prototype1/Assets/Scripts/Tab Scripts/Tab_slidePopup.cs
@@ -14,7 +14,7 @@ public class Tab_slidePopup : Tab_operatePopup
     [Header("")]
     [SerializeField] public PopupSlideDirection slideDirection = PopupSlideDirection.Horizontal;
     [Tooltip("You need to drag the parent popUp in here so that both the popup and the shadow will move")]
-    [SerializeField] private Transform slideMe;
+    [SerializeField] private List<Transform> slideMe;
     [Tooltip("How fast the popup moves relative to tab movement")]
     [SerializeField] private float movementSpeed = 1.0f;
     // --------------------------------------------------------------------------------------------------------------------------------------- INSPECTOR INTERFACE END
@@ -24,6 +24,7 @@ public class Tab_slidePopup : Tab_operatePopup
 
     private void Start()
     {
+        // retrieve the component from which we will read the input
         operateTabScript = GetComponent<Tab_operateTab>();
         if (operateTabScript == null)
         {
@@ -34,17 +35,24 @@ public class Tab_slidePopup : Tab_operatePopup
     // Update is called once per frame
     private void Update()
     {
+        // how much the tab moved/rotated since last frame
         tabMovement = operateTabScript.getTabMovement();
+
+        // if there has been any movement since last frame
         if (tabMovement != 0)
         {
-            switch (slideDirection)
+            if (slideMe.Count == 0) Debug.Log("You attached a slide pop up script but you did not give me any pop up to slide! :(");
+            // slide all the transforms in the appropriate direction (selected in the inspector)
+            else
             {
-                case PopupSlideDirection.Horizontal:
-                    slideMe.Translate(new Vector3(tabMovement * movementSpeed, 0, 0));
-                    break;
-                case PopupSlideDirection.Vertical:
-                    slideMe.Translate(new Vector3(0, 0, tabMovement * movementSpeed));
-                    break;
+                foreach (Transform popupTransform in slideMe)
+                {
+                    switch (slideDirection)
+                    {
+                        case PopupSlideDirection.Horizontal: popupTransform.Translate(new Vector3(tabMovement * movementSpeed, 0, 0)); break;
+                        case PopupSlideDirection.Vertical: popupTransform.Translate(new Vector3(0, 0, tabMovement * movementSpeed)); break;
+                    }
+                }
             }
         }
     }
